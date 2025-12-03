6c18091d5b30db5532b7ee2b71c268763d017300
김홍일 <zkfnth05@gmail.com>
2025-12-03 11:57:36 +0000

    지도 안보임

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  },
 };
 
 export default nextConfig;
