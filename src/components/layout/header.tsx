e98b865192fd665d8c030125e9b030557aef7833
김홍일 <zkfnth05@gmail.com>
2025-12-03 06:36:06 +0000

    더 크게

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center justify-center">
         <Link href="/" className="flex items-center gap-2">
-          <span className="font-headline text-2xl font-bold text-primary">
+          <span className="font-headline text-4xl font-bold text-primary">
             Aura
           </span>
         </Link>
