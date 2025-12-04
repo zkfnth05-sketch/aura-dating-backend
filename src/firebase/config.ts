5784a32ddaabca0c9e21d0bf3486c8ad215eefc9
김홍일 <zkfnth05@gmail.com>
2025-12-04 07:17:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "aura-ai-dating-38251551-64a99",
+  "appId": "1:865239923716:web:eb6833b2eaf6377e920951",
+  "apiKey": "AIzaSyDa6fe6vHmVRbeMDoBU44b7qKaI_c7s3h4",
+  "authDomain": "aura-ai-dating-38251551-64a99.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "865239923716"
+};
