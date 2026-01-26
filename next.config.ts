a6b3784820d03eab5bc33afe731ba1fe3b0c991c
김홍일 <zkfnth05@gmail.com>
2026-01-26 12:59:13 +0000

    먼저, 터미널을 열고 프로젝트의 루트 디렉터리에서 아래 명령어를 실행하여 VAPID 키를 생성해주세요.이거 잘 되어 있는지...확

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,7 @@ const nextConfig: NextConfig = {
   },
   env: {
     NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    NEXT_PUBLIC_VAPID_PUBLIC_KEY: process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY,
   },
 };
 
