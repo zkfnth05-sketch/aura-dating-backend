c679e9f70fa11baa04cd2a2f88e5560b2160637c
김홍일 <zkfnth05@gmail.com>
2026-02-02 22:51:05 +0000

    수정해줘...

--- android/app/src/main/java/app/web/aura/LauncherActivity.java
+++ android/app/src/main/java/app/web/aura/LauncherActivity.java
@@ -28,24 +28,14 @@ public class LauncherActivity
     
     @Override
     protected boolean shouldGoEdgeToEdge() {
-        return false;
+        return true;
     }
-
     
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         EdgeToEdge.enable(this);
         super.onCreate(savedInstanceState);
-        // Setting an orientation crashes the app due to the transparent background on Android 8.0
-        // Oreo and below. We only set the orientation on Oreo and above. This only affects the
-        // splash screen and Chrome will still respect the orientation.
-        // See https://github.com/GoogleChromeLabs/bubblewrap/issues/496 for details.
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT);
-        } else {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
-        }
     }
 
     @Override
--- android/src/main/java/app/web/aura/LauncherActivity.java
+++ android/src/main/java/app/web/aura/LauncherActivity.java
@@ -28,7 +28,7 @@ public class LauncherActivity
     
     @Override
     protected boolean shouldGoEdgeToEdge() {
-        return false;
+        return true;
     }
     
 
@@ -36,15 +36,6 @@ public class LauncherActivity
     protected void onCreate(Bundle savedInstanceState) {
         EdgeToEdge.enable(this);
         super.onCreate(savedInstanceState);
-        // Setting an orientation crashes the app due to the transparent background on Android 8.0
-        // Oreo and below. We only set the orientation on Oreo and above. This only affects the
-        // splash screen and Chrome will still respect the orientation.
-        // See https://github.com/GoogleChromeLabs/bubblewrap/issues/496 for details.
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT);
-        } else {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
-        }
     }
 
     @Override
